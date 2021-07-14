93664a63fdac0650c21ba722c35c3e6b28267df5
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-07-14 18:29:24 +0200

    Finished styling

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ let yScale;
 
 let width = 1200;
 let height = 600;
-let padding = 80;
+let padding = 100;
+let yearsRange;
 
 let svg = d3.select("svg");
 let legend = d3.select("legend");
@@ -20,6 +21,8 @@ function drawCanvas() {
 }
 
 function generateScales() {
+  yearsRange = d3.max(values, (d) => d.year) - d3.min(values, (d) => d.year);
+
   xScale = d3
     .scaleLinear()
     .domain([d3.min(values, (d) => d.year), d3.max(values, (d) => d.year)])
@@ -43,22 +46,29 @@ function drawCell() {
     .attr("data-temp", (d) => d.variance)
     .attr("x", (d) => xScale(d.year))
     .attr("y", (d) => yScale(new Date(0, d.month - 1, 0, 0, 0, 0, 0)))
-    .attr("width", "3px")
-    .attr("height", (height - 2 * padding) / 11)
+    .attr("width", (width - 2 * padding) / yearsRange)
+    .attr("height", (height - 2 * padding) / 12)
     .attr("fill", (d) => {
-      if (d.variance < -1) return "blue";
-      if (d.variance < 0) return "lightblue";
-      if (d.variance > 1) return "red";
-      if (d.variance >= 0) return "lightcoral";
+      if (d.variance < -2.5) return "#4575b4";
+      else if (d.variance < -0.5) return "#abd9e9";
+      else if (d.variance < 0.5) return "#ffffbf";
+      else if (d.variance < 2.5) return "#fdae61";
+      else if (d.variance > 2.5) return "#d73027";
     })
     .on("mouseover", (d) => {
       let year = d.year;
       tooltip
         .attr("data-year", year)
-        .style("left", d3.event.pageX - 35 + "px")
-        .style("top", d3.event.pageY - 70 + "px")
+        .style("left", d3.event.pageX - 100 + "px")
+        .style("top", Math.ceil((d3.event.pageY + 1) / 10) * 10 - 120 + "px")
         .style("visibility", "visible")
-        .text(d.variance.toFixed(1) + "°C");
+        .text(
+          `${d.year} ${parseMonth(d.month)} Temperature: ${(
+            baseTemperature + d.variance
+          ).toFixed(1)}°C Variance: ${
+            d.variance > 0 ? "+" + d.variance.toFixed(1) : d.variance.toFixed(1)
+          }°C`
+        );
     })
     .on("mouseout", (d) => tooltip.style("visibility", "hidden"));
 }
@@ -72,6 +82,14 @@ function generateAxes() {
     .attr("id", "x-axis")
     .attr("transform", `translate(0, ${height - padding})`);
 
+  svg
+    .append("text")
+    .attr("id", "x-axis-label")
+    .attr("x", width / 2)
+    .attr("y", height - 40)
+    .style("text-anchor", "middle")
+    .text("Years");
+
   let yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat("%B"));
 
   svg
@@ -79,6 +97,15 @@ function generateAxes() {
     .call(yAxis)
     .attr("id", "y-axis")
     .attr("transform", `translate(${padding}, 0)`);
+
+  svg
+    .append("text")
+    .attr("id", "y-axis-label")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 25)
+    .attr("x", 0 - height / 2)
+    .style("text-anchor", "middle")
+    .text("Months");
 }
 
 fetch(url)
@@ -86,10 +113,39 @@ fetch(url)
   .then((data) => {
     values = data.monthlyVariance;
     baseTemperature = data.baseTemperature;
-    console.log(values);
-    console.log(baseTemperature);
+    //console.log(values);
+    //console.log(baseTemperature);
     drawCanvas();
     generateScales();
     drawCell();
     generateAxes();
   });
+
+function parseMonth(num) {
+  switch (num) {
+    case 1:
+      return "January";
+    case 2:
+      return "February";
+    case 3:
+      return "March";
+    case 4:
+      return "April";
+    case 5:
+      return "May";
+    case 6:
+      return "June";
+    case 7:
+      return "July";
+    case 8:
+      return "August";
+    case 9:
+      return "September";
+    case 10:
+      return "October";
+    case 11:
+      return "November";
+    case 12:
+      return "December";
+  }
+}
